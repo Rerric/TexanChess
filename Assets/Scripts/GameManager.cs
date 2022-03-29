83aefce62411055287660c2dc6cf157b4d6bcb62
Rerric <79161388+Rerric@users.noreply.github.com>
2022-03-29 02:44:49 -0400

    Initialized Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Game Initialization
+    public int Players;
+
+    public int turn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turn = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
