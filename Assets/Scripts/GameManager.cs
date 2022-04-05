41e6759d5c8888cc08bf926393bd90a74df8bc7c
Rerric <79161388+Rerric@users.noreply.github.com>
2022-04-05 15:11:54 -0400

    Spawner Fully Functional
    Pieces now spawn at each team's spawnpoints! The first piece to spawn is designated as the King, then the other pieces spawn around it. The distance from the king is a variable called Spawn Offset.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,12 +14,22 @@ public class GameManager : MonoBehaviour
 
     public int turn; //what team's turn it currently is
 
-    //public List<int> pieces = new List<int>(); //stores piece ID's
+    public GameObject[] pieces;
+
+    public List<int> pieceIDs = new List<int>(); //stores piece ID's
 
     public Transform[] pieceTransforms;
 
-    //Important Scripts to communicate with
+    //Important Scripts & Objects to communicate with
     public CameraController cameraScript;
+
+    public GameObject spawnPoint1; //Team 1 Spawn
+    public GameObject spawnPoint2; //Team 2 Spawn
+    private Vector3 spawn;
+    public float spawnOffset; //Determines how far away pieces spawn from the king
+
+    public Vector3[] spawnPoints1 = new Vector3[5];
+    public Vector3[] spawnPoints2 = new Vector3[5];
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +44,7 @@ public class GameManager : MonoBehaviour
 
         cameraScript = GameObject.Find("Third Person Camera").GetComponent<CameraController>();
 
+        FindSpawnPoints();
         GameStart();
     }
 
@@ -45,18 +56,83 @@ public class GameManager : MonoBehaviour
 
     void GameStart()
     {
-        //SpawnTeams();
+        SpawnTeams(teams);
     }
 
-    /*void SpawnTeams(int teams)
-     *  {
-     *      //find team spawnpoints based on the current level
-     *      //determine number of pieces to spawn
-     *      //spawn pieces accordingly
-     *      //Grant piece ID's based on order spawned
-     *      //Add their ID's to the List (pieces)
-     *  }
-    */
+    void SpawnTeams(int teams)
+    {
+        //Get Spawn positions
+        var spawn1 = spawnPoints1[0];
+        var spawn2 = spawnPoints2[0];
+        
+
+        //Spawn King for each team
+        for (var i = 0; i < teams; i++)
+        { 
+            if (i == 0) spawn = spawn1;
+            if (i == 1) spawn = spawn2;
+
+            var id = i + 1;
+
+            var king = Instantiate(pieces[0], spawn, transform.rotation);
+            var kingTransform = king.GetComponent<Transform>();
+            var kingScript = king.GetComponent<Piece>();
+            kingScript.isKing = true;
+            kingScript.team = id;
+            kingScript.pieceID = id;
+            pieceIDs.Add(id);
+
+            var counter = 1;
+
+            for (var e = id + 2; e < 11; e += 2) //Spawn 4 pieces for each team
+            {
+                var _id = e;
+
+                if (id == 1) //Spawn piece for team 1
+                {
+                    var piece = Instantiate(pieces[0], spawnPoints1[counter], transform.rotation);
+                    var pieceTransform = piece.GetComponent<Transform>();
+                    var pieceScript = piece.GetComponent<Piece>();
+                    pieceScript.team = id;
+                    pieceScript.pieceID = _id;
+                    pieceIDs.Add(_id);
+                }
+
+                if (id == 2) //Spawn piece for team 2
+                {
+                    var piece = Instantiate(pieces[0], spawnPoints2[counter], transform.rotation);
+                    var pieceTransform = piece.GetComponent<Transform>();
+                    var pieceScript = piece.GetComponent<Piece>();
+                    pieceScript.team = id;
+                    pieceScript.pieceID = _id;
+                    pieceIDs.Add(_id);
+                }
+
+                counter += 1;
+
+            }   
+        }
+        
+
+
+    }
+
+    void FindSpawnPoints()
+    {
+        //Team 1
+        spawnPoints1[0] = spawnPoint1.transform.position;
+        spawnPoints1[1] = new Vector3(spawnPoints1[0].x, spawnPoints1[0].y, spawnPoints1[0].z + spawnOffset);
+        spawnPoints1[2] = new Vector3(spawnPoints1[0].x, spawnPoints1[0].y, spawnPoints1[0].z - spawnOffset);
+        spawnPoints1[3] = new Vector3(spawnPoints1[0].x - spawnOffset / 2, spawnPoints1[0].y, spawnPoints1[0].z + spawnOffset / 2);
+        spawnPoints1[4] = new Vector3(spawnPoints1[0].x - spawnOffset / 2, spawnPoints1[0].y, spawnPoints1[0].z - spawnOffset / 2);
+
+        //Team 2
+        spawnPoints2[0] = spawnPoint2.transform.position;
+        spawnPoints2[1] = new Vector3(spawnPoints2[0].x, spawnPoints2[0].y, spawnPoints2[0].z + spawnOffset);
+        spawnPoints2[2] = new Vector3(spawnPoints2[0].x, spawnPoints2[0].y, spawnPoints2[0].z - spawnOffset);
+        spawnPoints2[3] = new Vector3(spawnPoints2[0].x + spawnOffset / 2, spawnPoints2[0].y, spawnPoints2[0].z + spawnOffset / 2);
+        spawnPoints2[4] = new Vector3(spawnPoints2[0].x + spawnOffset / 2, spawnPoints2[0].y, spawnPoints2[0].z - spawnOffset / 2);
+    }
 
     void UpdateCamera()
     {
