7fdea829e80d55d1c6aa73168280eb7245451a53
Rerric <79161388+Rerric@users.noreply.github.com>
2022-04-26 01:03:07 -0400

    Ricocheting Bullets Implemented
    Works great!

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,25 +10,44 @@ public class Bullet : MonoBehaviour
 
     public float speed;
     Vector3 velocity;
+    Vector3 movementDirection;
     public float gravity = -9.81f; //note this only affects the piece while the controller is enabled
+    public float gravityOffset;
+
+    public int bounces;
+
+    private AudioManager audioScript;
+    public AudioClip[] hitSounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.AddForce(transform.right * speed, ForceMode.Impulse);
+        audioScript = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //gravity
-        velocity.y += 1;
+        rb.AddForce(Physics.gravity * rb.mass * gravityOffset);
+
+        movementDirection = rb.velocity;
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection.normalized, Vector3.forward);
+
+            transform.rotation = toRotation; //Rotate in direction of movement
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+
         if (collision.gameObject.layer == 6) //if it's a non-player piece
         {
+            audioScript.PlaySoundPyramind(hitSounds[1], gameObject);
             var hit = collision.gameObject.GetComponent<Piece>();
             hit.health -= damage;
             hit.ImHit();
@@ -36,7 +55,20 @@ public class Bullet : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject, 2.0f);
+            audioScript.PlaySoundPyramind(hitSounds[0], gameObject);
         }
+
+        if (bounces == 0) Destroy(gameObject);
+        bounces -= 1;
+
+        var _speed = movementDirection.magnitude;
+        var direction = Vector3.Reflect(movementDirection.normalized, collision.contacts[0].normal);
+
+        rb.velocity = direction * speed;
+
+        gravityOffset += 0.2f;
+
+        Destroy(gameObject, 10f);
+        
     }
 }
