3e80de5c195718e2f5fabc4f6aa33ec5baab7411
Rerric <79161388+Rerric@users.noreply.github.com>
2022-05-11 04:19:47 -0400

    Main Menu WIP

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour
+public class Button : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     public int id;
 
     private MainMenu menuScript;
 
-    public bool isHovered;
+    public Image image;
+
+    public bool isSelected;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +20,17 @@ public class Button : MonoBehaviour
         menuScript = GameObject.Find("MainMenuManager").GetComponent<MainMenu>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (isSelected)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+        }
+        else
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0.4f);
+        }
     }
 
     public void UseButton()
@@ -43,5 +54,18 @@ public class Button : MonoBehaviour
         {
 
         }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (eventData.selectedObject == this.gameObject)
+        {
+            isSelected = true;
+        }
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+            isSelected = false;
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,8 +10,6 @@ public class MainMenu : MonoBehaviour
 
     Vector2 choose;
 
-    public GameObject[] buttons; //0 = Start; 1 = How-To; 2 = Options; 3 = Credits;
-
     public int currentSelection;
 
     // Start is called before the first frame update
@@ -26,7 +24,6 @@ public class MainMenu : MonoBehaviour
         controls.Paused.Choose.performed += ctx => ChooseSelection(ctx.ReadValue<Vector2>().y);
         controls.Paused.Choose.canceled += ctx => choose = Vector2.zero;
         controls.Paused.Quit.performed += ctx => Application.Quit();
-        controls.Paused.Select.performed += ctx => Select();
 
         currentSelection = 0;
     }
@@ -34,7 +31,7 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateButtonUI();
+        
     }
 
     void ChooseSelection(float dir)
@@ -43,32 +40,10 @@ public class MainMenu : MonoBehaviour
         currentSelection -= _dir;
         if (currentSelection > 3) currentSelection = 0;
         if (currentSelection < 0) currentSelection = 3;
-        Debug.Log(currentSelection);
-    }
-
-    void Select()
-    {
-        var button = buttons[currentSelection].GetComponent<Button>();
-        button.UseButton();
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
-    }
-
-    void UpdateButtonUI()
-    {
-        for (var i = 0; i < buttons.Length; i++)
-        {
-            var button = buttons[i].GetComponent<Button>();
-            var buttonImage = button.GetComponent<Image>();
-            
-            if (button.id == currentSelection)
-            {
-                buttonImage.color = new Color(buttonImage.color.r, buttonImage.color.g, buttonImage.color.b, 1f);
-            }
-            else buttonImage.color = new Color(buttonImage.color.r, buttonImage.color.g, buttonImage.color.b, 0.5f);
-        }
     }
 }
